f8ed746ad49fe5108b303c496f690195e2026afd
Nameless <unfortunate42@gmail.com>
2012-02-25 21:16:50 +0200

    Finished renaming project, some javascript structural changes.

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for ongoing-canvas project.
+# Django settings for ongoing_canvas project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -100,13 +100,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'ongoing-canvas.urls'
+ROOT_URLCONF = 'ongoing_canvas.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/nameless/programs/ongoing-canvas/templates",
+    "/home/nameless/programs/ongoing_canvas/templates",
 )
 
 INSTALLED_APPS = (
