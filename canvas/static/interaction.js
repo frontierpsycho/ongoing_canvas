e990621744eb3a2845e80446c544769fce3425e2
Nameless <unfortunate42@gmail.com>
2013-04-18 04:43:49 +0300

    Gave popup the feeling color.

--- canvas/static/interaction.js
+++ canvas/static/interaction.js
@@ -1,4 +1,9 @@
-function add_interaction(element, id) {
+function add_interaction(element, id, colour) {
+	// HACKISH add percentage sign after second and third value
+	colourParts = colour.split(',');
+	colourParts[1] += "%";
+	colourParts[2] = colourParts[2].substring(0,colourParts[2].length-1)+"%)";
+	colour = colourParts.join(',');
 	element.hover(
 			function(e) {
 				this.gl = this.glow({ color: "#CCC", width: 5 });
@@ -7,11 +12,12 @@ function add_interaction(element, id) {
 				this.gl.remove();
 			})
 	.click(function(e) {
-		$('#popup').bPopup({
+		$('div#popup').css('background-color', colour).css('opacity', '0.9')
+		.bPopup({
 			contentContainer: ".content",
 			loadUrl: '/canvas/feeling/'+id+'/', //Uses jQuery.load()
 			position: ['auto','auto'],
 			positionStyle: 'fixed'
-		});
+		});	
 	});
 }
